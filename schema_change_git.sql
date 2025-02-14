bf027e00f51f7254d496dc1b14abe8811e8ebe8f
Elan <a932327192@gmail.com>
2025-02-14 17:31:02 -0500

    fix sql

--- schema_change_git.sql
+++ schema_change_git.sql
@@ -3,5 +3,5 @@ USE companydb;
 CREATE TABLE departments (
     `department_id` INT AUTO_INCREMENT PRIMARY KEY,
     `department_name` VARCHAR(255) NOT NULL,
-    `location` varchar(255),
+    `location` varchar(255)
 );
